d173f34ddf69a21805fd4b4f2721dbdc23cc0b40
Jaya Ganguly <110370085+Pseudocoder000@users.noreply.github.com>
2026-04-10 04:12:39 +0530

    bugs fixed

--- Frontend/src/components/layout/Layout.jsx
+++ Frontend/src/components/layout/Layout.jsx
@@ -5,11 +5,11 @@ import BottomNav from './BottomNav'
 
 export default function Layout() {
   return (
-    <div className="flex flex-col md:flex-row h-screen overflow-hidden">
+    <div className="flex flex-col md:flex-row min-h-screen">
     <Sidebar />
       <div className="flex flex-col flex-1 overflow-hidden min-w-0">
         <Navbar />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6">
+       <main className="flex-1 overflow-y-auto p-4 md:p-6" style={{ scrollbarWidth: 'none' }}>
           <Outlet />
         </main>
         <BottomNav />
